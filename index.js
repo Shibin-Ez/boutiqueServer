46e7987417d19a523d3b1685e2aaee4e08298ad3
Shibin-Ez <ezshibin@gmail.com>
2025-04-28 17:50:31 +0530

    json limit removed4

--- index.js
+++ index.js
@@ -31,17 +31,6 @@ import { getUser } from "./controllers/user.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(express.json({ limit: "220mb" }));
-app.use(express.urlencoded({ limit: '220mb', extended: true }));
-// app.use(express.json());
-app.use(helmet()); // if not used with helmet, cors will not work
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(cors());
-app.use("/public", express.static("public"));
-
-// If you're behind a proxy, this will tell Express to use the X-Forwarded-For header.
-app.set("trust proxy", true);
 
 // FILE STORAGE
 const storage = multer.diskStorage({
@@ -64,6 +53,16 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+// If you're behind a proxy, this will tell Express to use the X-Forwarded-For header.
+app.set("trust proxy", true);
+
+// MIDDLEWARES
+app.use(helmet()); // if not used with helmet, cors will not work
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+app.use("/public", express.static("public"));
+
 // ROUTES WITH FILES
 app.post("/shops", authenticate, upload.single("profilePic"), createShop);
 app.post(
@@ -78,6 +77,10 @@ app.post(
   ]),
   createPost
 );
+
+// BODY PARSER
+app.use(express.json({ limit: "220mb" }));
+app.use(express.urlencoded({ limit: '220mb', extended: true }));
 
 // ROUTES
 app.use("/shops", shopRoutes);
